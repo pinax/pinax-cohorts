d8ccd41be607927ab8400153f53f4718ca6f3dfa
Patrick Altman <paltman@gmail.com>
2016-05-12 16:24:42 -0500

    Initial commit
    Started from a7be5e51dac2221637300d070c1dbbb2b7f7e930 of pinax-starter-app

---
+++ pinax/cohorts/__init__.py
@@ -0,0 +1,5 @@
+import pkg_resources
+
+
+default_app_config = "pinax.cohorts.apps.AppConfig"
+__version__ = pkg_resources.get_distribution("pinax-cohorts").version
---
+++ pinax/cohorts/apps.py
@@ -0,0 +1,11 @@
+import importlib
+
+from django.apps import AppConfig as BaseAppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class AppConfig(BaseAppConfig):
+
+    name = "pinax.cohorts"
+    label = "pinax_cohorts"
+    verbose_name = _("Pinax Cohorts")
